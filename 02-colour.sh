c53fd868d930aa555f2670005b723a094cf526db
87saswat <87saswat@gmail.com>
2023-06-06 15:21:40 +0530

    color presentation

--- 02-colour.sh
+++ 02-colour.sh
@@ -6,3 +6,8 @@ echo -e "\e[32m I am printing a colour \e[0m"
 echo -e "\e[33m I am printing another colour \e[0m"
 echo -e "\e[34m Helo I am another colour \e[0m"
 echo -e "\e[35m Thus we can print different colours \e[0m"
+
+# Changing background colour
+
+echo -e "\e[43;32m Background with different coulor text \e[0m"
+echo -e "\e[44;35m Another background withh another colur text \e[0m"
