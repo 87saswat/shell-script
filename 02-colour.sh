064e399531824b6d86a075f4e3f38d225249730a
87saswat <87saswat@gmail.com>
2023-06-06 15:17:34 +0530

    color presentation

---
+++ 02-colour.sh
@@ -0,0 +1,8 @@
+#!bin/bash
+
+#Colour output for better presentation
+
+echo -e "\e[32m I am printing a colour \e[0m"
+echo -e "\e[33m I am printing another colour \e[0m"
+echo -e "\e[34m Helo I am another colour \e[0m"
+echo -e "\e[35m Thus we can print different colours \e[0m"
