da6d40d46b8fcda6b209105cb285f5e3518b7ed1
87saswat <87saswat@gmail.com>
2023-06-07 13:57:48 +0530

    Automating through shellscript

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ echo starting the script
 ID=$(id -u)
 echo $ID
 
-if [$ID ne 0]; then
+if [$ID ne 1000]; then
 echo -e "\e[31m You need to be a root user and sudo previlage t run this script \e[0m"
 exit 1
 
