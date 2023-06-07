cd0383bcf7adbaca507e62d871748d896bb3531d
87saswat <87saswat@gmail.com>
2023-06-07 15:26:41 +0530

    Automating through shellscript

--- 08-if-else-condition.sh
+++ 08-if-else-condition.sh
@@ -1,12 +1,26 @@
 #!bin/bash
 
-ACTION=$1
+# ACTION=$1
 
-if [ "$ACTION" == "start" ]; then
-    echo -e "\e[31m Server starting \e[0m"
-    exit 0
+# if [ "$ACTION" == "start" ]; then
+#     echo -e "\e[31m Server starting \e[0m"
+#     exit 0
 
-else 
-    echo -e "\e[33m Server is not running \e[0m"
-    exit 1    
+# else 
+#     echo -e "\e[33m Server is not running \e[0m"
+#     exit 1    
+# fi
+
+
+VAR=$1
+
+if [ "$VAR" le 10 ]; then
+    echo "Value is too small"
+elif ["$VAR" le 20 ]; then
+    echo "Value is on medium range"
+elif [ "$VAR" le 100 ]; then
+    echo "WHOAA !! a Big number"
+
+else
+    echo "Beyond our immagination"    
 fi
