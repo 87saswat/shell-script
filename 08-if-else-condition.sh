c788c027860a19191b530d1c2c03b8be9b0dc4a5
87saswat <87saswat@gmail.com>
2023-06-07 15:19:44 +0530

    Automating through shellscript

--- 08-if-else-condition.sh
+++ 08-if-else-condition.sh
@@ -4,7 +4,9 @@ ACTION=$1
 
 if ["$ACTION" == "start"]; then
     echo -e "\e[31m Server starting \e[0m"
+    exit 0
 
 else 
-    echo -e "\e[33m Server is not running \e[0m"    
+    echo -e "\e[33m Server is not running \e[0m"
+    exit 1    
 fi
