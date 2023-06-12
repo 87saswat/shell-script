030ea850bbc1837a042e22db880bdd6ab21540f2
87saswat <87saswat@gmail.com>
2023-06-12 22:24:43 +0530

    Automating roboshop on EC2

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,2 +1,17 @@
 #!bin/bash
-echo I am fronend
+echo "Installing nginix:"
+
+yum install nginix
+
+# yum install nginx -y
+# systemctl enable nginx
+# systemctl start nginx
+# curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+#Deploy in Nginx Default Location.
+# cd /usr/share/nginx/html
+# rm -rf *
+# unzip /tmp/frontend.zip
+# mv frontend-main/* .
+# mv static/* .
+# rm -rf frontend-main README.md
+# mv localhost.conf /etc/nginx/default.d/roboshop.conf
