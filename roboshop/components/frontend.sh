7fc18146a7ac02e7d0d5afa21ce989a984fe7119
87saswat <87saswat@gmail.com>
2023-06-13 20:57:44 +0530

    Automating frontend

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -7,10 +7,10 @@ if [ $ID -ne 0 ] ; then
 fi   
 stat() {
     if [ $1 -eq 0 ]; then
-    echo -e "\e[32m Intsllation success \e[0m"
+    echo -e "\e[32m success \e[0m"
 
 else
-    echo -e "\e[31m Installation failed \e[0m"
+    echo -e "\e[31m failed \e[0m"
     exit 2
 fi
 }
