e4bb80ef1a2294ed95ff6364eb341f98335aa2e8
87saswat <87saswat@gmail.com>
2023-06-13 20:56:32 +0530

    Automating frontend

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -5,33 +5,30 @@ if [ $ID -ne 0 ] ; then
     echo -e "\e[31m This script is expected is run by a root user or with sudo previlage \e[0m"
     exit 1
 fi   
-echo -n "Installing nginx...." 
-yum install nginx -y &>> "/tmp/${COMPONENT}.log"
-
-if [ $? -eq 0 ]; then
+stat() {
+    if [ $1 -eq 0 ]; then
     echo -e "\e[32m Intsllation success \e[0m"
 
 else
     echo -e "\e[31m Installation failed \e[0m"
+    exit 2
 fi
+}
+
+echo -n "Installing nginx...." 
+yum install nginx -y &>> "/tmp/${COMPONENT}.log"
+
+
 
 echo -n "Downloading ${COMPONENT} component ................."
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
-if [ $? -eq 0 ]; then
-    echo -e "\e[32m Download success \e[0m"
-else    echo -e "\e[31m Download failed \e[0m"
-
-fi
+stat $?
 
 echo -n "Performing cleanup...."
 cd /usr/share/nginx/html
 rm -rf * &>> "/tmp/${COMPONENT}.log"
 
-if [ $? -eq 0 ]; then
-    echo -e "\e[32m Cleanup success \e[0m"
-else    echo -e "\e[31m Cleanup failed \e[0m"
-
-fi
+stat $?
 
 # systemctl enable nginx
 # systemctl start nginx
