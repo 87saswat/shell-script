4e110540e125239586830f70d9ee2140507a4fd2
87saswat <87saswat@gmail.com>
2023-06-13 20:36:42 +0530

    Automating frontend

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -28,8 +28,8 @@ cd /usr/share/nginx/html
 rm -rf *  &>>  "/tmp/${COMPONENT}.log"
 
 if [ $? -eq 0 ]; then
-    echo -e "\e[32m Download ..... success \e[0m"
-else    echo -e "\e[31m Download .... failed \e[0m"
+    echo -e "\e[32m Cleanup ..... success \e[0m"
+else    echo -e "\e[31m Cleanup .... failed \e[0m"
 
 fi
 
