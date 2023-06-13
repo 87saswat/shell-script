3820847a74bc4e1e6394eaf5395d3f391086ee18
87saswat <87saswat@gmail.com>
2023-06-13 20:28:37 +0530

    Automating frontend

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -4,7 +4,8 @@ ID=$(id -u)
 if [ $ID -ne 0 ] ; then
     echo -e "\e[31m This script is expected is run by a root user or with sudo previlage \e[0m"
     exit 1
-fi    
+fi   
+echo -n "Installing nginx...." 
 yum install nginx -y &>> "/tmp/${COMPONENT}.log"
 
 if [ $? -eq 0 ]; then
@@ -14,11 +15,11 @@ else
     echo -e "\e[31m Installation failed \e[0m"
 fi
 
-echo "Downloading ${COMPONENT} component"
+echo -n "Downloading ${COMPONENT} component ................."
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
 if [ $? -eq 0 ]; then
-    echo -e "\e[32m Installation success \e[0m"
-else    echo -e "\e[31m Installation failed \e[0m"
+    echo -e "\e[32m Downloading ..... success \e[0m"
+else    echo -e "\e[31m Download .... failed \e[0m"
 
 fi
 
