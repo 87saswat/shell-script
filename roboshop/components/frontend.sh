1ed9549b7eac99af27612edca2882e37f62a6600
87saswat <87saswat@gmail.com>
2023-06-13 20:40:01 +0530

    Automating frontend

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -25,7 +25,7 @@ fi
 
 echo -n "Performing cleanup...."
 cd /usr/share/nginx/html
-rm -rf *  &>>  "/tmp/${COMPONENT}.log"
+rm -rf * &>> "/tmp/${COMPONENT}.log"
 
 if [ $? -eq 0 ]; then
     echo -e "\e[32m Cleanup success \e[0m"
